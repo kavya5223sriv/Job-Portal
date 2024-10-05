c0c45eb783dadd0c7106542bcde7deb5d84c65b5
srivastava_kavya775 <116947598+kavya5223sriv@users.noreply.github.com>
2024-10-05 23:44:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/Navbar"
+import Navbar from "./components/NavBar"
 import Header from "./components/Header"
 import SearchBar from "./components/SearchBar"
 import JobCard from "./components/JobCard"
